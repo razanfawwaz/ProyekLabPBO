bcb84d1e63c776fb57afdd7bbb67a33d726c0a77
razanfawwaz <razanfawwaz1905@gmail.com>
2021-11-29 17:04:59 +0700

    making a form

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,19 @@ import javax.swing.*;
 
 public class Main {
     private JPanel panel;
-    private JButton exit;
+    private JButton exitButton;
+    private JTable table1;
+    private JTable table2;
+    private JCheckBox cepatSajiCheckBox;
+    private JCheckBox sehatCheckBox;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JCheckBox asamCheckBox;
+    private JTextField textField3;
+    private JCheckBox manisCheckBox;
+    private JTextField textField4;
+    private JCheckBox panasCheckBox;
+    private JTextField textField5;
+    private JCheckBox dinginCheckBox;
+    private JTextField textField6;
 }
